65f2b9ff4b70ffe7390d13241563264cd2f12784
zainab almutawah <zainab214000@gmail.com>
2024-03-06 14:54:21 +0000

    commit

--- models/staging/raw/stg_raw__criteo.sql
+++ models/staging/raw/stg_raw__criteo.sql
@@ -9,6 +9,13 @@ source as (
 renamed as (
 
     select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name AS campaing_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
+        impression,
+        click
 
     from source
 
--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -9,6 +9,13 @@ source as (
 renamed as (
 
     select
+        date_date,
+        paid_source,
+        campaign_key,
+        campgn_name,
+        ads_cost,
+        impression,
+        click
 
     from source
 
